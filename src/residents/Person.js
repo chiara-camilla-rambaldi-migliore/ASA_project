448ddb142dd27cc2d916fec715955fe748560a75
chiara-camilla-rambaldi-migliore <cami.rambaldi@hotmail.it>
2022-04-27 11:01:46 +0200

    removed useless set

--- src/residents/Person.js
+++ src/residents/Person.js
@@ -5,7 +5,6 @@ class Person extends Observable {
         super(init)
         this.house = house
         this.name = name
-        this.set('in_room', 'bedroom')
     }
     moveTo (to) {
         if ( to in this.house.rooms[this.in_room].doors_to ) {
